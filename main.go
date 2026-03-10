ee538290a37fa788b9254ffc3c1ada5c03dbf66f
kh4f <kh4f.dev@gmail.com>
2026-03-10 16:20:35 +0300

    refactor(project): migrate from Rust to Go implementation
    - Remove Rust project files including `Cargo.toml`, `Cargo.lock`, and source files
    - Add Go project files including `go.mod` and `go.sum`
    - Implement GPU temperature reading in Go using `golang.org/x/sys/windows`

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import ("fmt"; "log"; "unsafe"; w "golang.org/x/sys/windows")
+
+type ADLTemperature struct {
+    Size int32
+    Temperature int32
+}
+
+var (
+	dll = w.NewLazySystemDLL("atiadlxx.dll")
+	ADL_Main_Control_Create = dll.NewProc("ADL_Main_Control_Create").Call
+	ADL_Main_Control_Destroy = dll.NewProc("ADL_Main_Control_Destroy").Call
+	ADL_Adapter_NumberOfAdapters_Get = dll.NewProc("ADL_Adapter_NumberOfAdapters_Get").Call
+	ADL_Overdrive5_Temperature_Get = dll.NewProc("ADL_Overdrive5_Temperature_Get").Call
+)
+
+func main() {
+	adlMalloc := w.NewCallback(func(size int32) uintptr {
+		ptr, _ := w.LocalAlloc(0, uint32(size))
+		return uintptr(ptr)
+	})
+	ADL_Main_Control_Create(adlMalloc, 1)
+    defer ADL_Main_Control_Destroy()
+
+    var adapters int32
+	ADL_Adapter_NumberOfAdapters_Get(uintptr(unsafe.Pointer(&adapters)))
+    if adapters == 0 { log.Fatal("GPU adapters not found") }
+    fmt.Println("Adapters found:", adapters)
+
+    temp := ADLTemperature{ Size: int32(unsafe.Sizeof(ADLTemperature{})) }
+    r, _, _ := ADL_Overdrive5_Temperature_Get(0, 0, uintptr(unsafe.Pointer(&temp)))
+    if r == 0 {
+        fmt.Printf("GPU Temperature: %d°C\n", temp.Temperature/1000)
+    } else {
+        fmt.Println("Temperature read failed")
+    }
+}
