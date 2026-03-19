3d85fb7ff4ef7c9089c8dfc7fec2ee9b894bce7c
kh4f <kh4f.dev@gmail.com>
2026-03-20 01:22:36 +0300

    chore(build): add short CLI flags and formatting option
    Introduce short-form flags for build commands and add a new `-f` flag to run `go fmt` across the project. This streamlines the build process and improves developer ergonomics.

--- build.go
+++ build.go
@@ -12,14 +12,16 @@ func main() {
 	var cmd string
 
 	switch {
-	case slices.Contains(os.Args, "--build"):
+	case slices.Contains(os.Args, "-b"):
 		cmd = "cd cmd/amdalert && " +
 			"windres res.rc -O coff -o res.syso && " +
 			"go build -o ../../AMDAlert.exe ."
-	case slices.Contains(os.Args, "--run"):
+	case slices.Contains(os.Args, "-r"):
 		cmd = "go run ./cmd/amdalert"
-	case slices.Contains(os.Args, "--release"):
-		cmd = "bunx relion -b internal/cli/cli.go"
+	case slices.Contains(os.Args, "-f"):
+		cmd = "go fmt ./..."
+	case slices.Contains(os.Args, "-l"):
+		cmd = "bunx relion"
 	}
 
 	execCmd := exec.Command("bash", "-c", cmd)
